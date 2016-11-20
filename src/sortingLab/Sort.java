12446ecfe162c8eabbcf3dc33cc57ed3564ab4c3
Greg Fletcher <gnfletcher@gmail.com>
2016-11-20 16:03:49 -0500

    Bubble Complete

--- src/sortingLab/Sort.java
+++ src/sortingLab/Sort.java
@@ -8,44 +8,62 @@ package sortingLab;
  *
  */
 public class Sort {
-  
-  public Sort(){
-    
-  }
-  
-  public static <T> T[] quickSortRecur(T[] array){
-    return array;
-  }
-  
-  public static <T> T[] quickSortIter(T[] array){
-    return array;
-  }
 
-  public static <T> T[] bubbleSort(T[] array){
-    return array;
-  }
-  
-  public static <T> T[] bucketSort(T[] array){
-    return array;
-  }
-  
-  public static <T> T[] heapSort(T[] array){
-    return array;
-  }
-  
-  public static <T> T[] insertionSort(T[] array){
-    return array;
-  }
-  
-  public static <T> T[] mergeSort(T[] array){
-    return array;
-  }
-  
-  public static <T> T[] radixSort(T[] array){
-    return array;
-  }
-  
-  public static <T> T[] treeSort(T[] array){
-    return array;
-  }
+	public Sort() {
+
+	}
+
+	public static Comparable[] quickSortRecur(Comparable[] array) {
+		return array;
+	}
+
+	public static Comparable[] quickSortIter(Comparable[] array) {
+		return array;
+	}
+
+	public static Comparable[] bubbleSort(Comparable[] array) {
+		boolean unordered = false;
+		do {
+			for (int i = 0; i < array.length; i++) {
+				if (array[i].compareTo(array[i + 1]) > 0) {
+					Comparable temp = array[i + 1];
+					array[i + 1] = array[i];
+					array[i] = temp;
+					unordered = true;
+				} else {
+					unordered = false;
+				}
+			}
+		} while (unordered);
+		return array;
+	}
+
+	public static Comparable[] bucketSort(Comparable[] array) {
+		return array;
+	}
+
+	public static Comparable[] heapSort(Comparable[] array) {
+		return array;
+	}
+
+	public static Comparable[] insertionSort(Comparable[] array) {
+		Comparable p = array[0];
+		boolean unordered = false;
+		do{
+			
+		}while (unordered);
+		return array;
+	}
+
+	public static Comparable[] mergeSort(Comparable[] array) {
+		return array;
+	}
+
+	public static Comparable[] radixSort(Comparable[] array) {
+		return array;
+	}
+
+	public static Comparable[] treeSort(Comparable[] array) {
+		return array;
+	}
 }
---
+++ src/sortingLab/driver.java
@@ -0,0 +1,9 @@
+package sortingLab;
+
+public class driver {
+	public static void main(String[] args){
+		
+		
+	}
+
+}
