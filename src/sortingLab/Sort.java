d5bedc2bbfcede66f0616ce1bceeb252951aa588
Greg Fletcher <gnfletcher@gmail.com>
2016-11-26 08:17:05 -0500

    QuickSort changes.

--- src/sortingLab/Sort.java
+++ src/sortingLab/Sort.java
@@ -17,22 +17,28 @@ public class Sort {
 		Comparable partition = array[0];
 		Comparable[] lessThan = new Comparable[array.length];
 		Comparable[] greaterThan = new Comparable[array.length];
-		Comparable[] sorted = new Comparable[array.length];
-		int i= 0;
-		while(array[i].compareTo(partition) < 0){
-			lessThan[i] = array[i];
-			i++;
+		int i = 0;
+		if (array.length > 0) {
+			while (array[i].compareTo(partition) <= 0) {
+				lessThan[i] = array[i];
+				i++;
+			}
+			while (i < array.length) {
+				greaterThan[i] = array[i];
+				i++;
+			}
+			quickSortRecur(greaterThan);
+			return lessThan;
 		}
-		i++;
-		while(i < array.length){
-			greaterThan[i] = array[i];
-			i++;
+		Comparable[] sorted = new Comparable[array.length];
+		for(int j = 0; j < lessThan.length; j++){
+			
 		}
 		return array;
 	}
 
 	public static Comparable[] quickSortIter(Comparable[] array) {
-	
+
 		return array;
 	}
 
@@ -98,18 +104,18 @@ public class Sort {
 	}
 
 	public static Comparable[] mergeSort(Comparable[] array) {
-		Comparable[] first = new Comparable[array.length/2];
-		Comparable[] second = new Comparable[array.length/2];
-		if(array.length > 2){
-			first = new Comparable[array.length/2];
-			second = new Comparable[array.length/2];
+		Comparable[] first = new Comparable[array.length / 2];
+		Comparable[] second = new Comparable[array.length / 2];
+		if (array.length > 2) {
+			first = new Comparable[array.length / 2];
+			second = new Comparable[array.length / 2];
 			System.arraycopy(array, 0, first, 0, first.length);
 			System.arraycopy(array, first.length, second, 0, second.length);
 			first = mergeSort(first);
 			second = mergeSort(second);
 		} else {
-			if(array.length == 2){
-				if(array[0].compareTo(array[1]) > 0){
+			if (array.length == 2) {
+				if (array[0].compareTo(array[1]) > 0) {
 					Comparable[] sorted = new Comparable[array.length];
 					sorted[0] = array[1];
 					sorted[1] = array[0];
@@ -122,8 +128,8 @@ public class Sort {
 		Comparable[] newArray = new Comparable[array.length];
 		int i = 0;
 		int j = 0;
-		for(int k = 0 ; k < newArray.length; k ++){
-			if(first[i].compareTo(second[j]) > 0){
+		for (int k = 0; k < newArray.length; k++) {
+			if (first[i].compareTo(second[j]) > 0) {
 				newArray[k] = first[i];
 				i++;
 			} else {
@@ -138,15 +144,15 @@ public class Sort {
 		int[] groups = new int[10];
 		int[] sorted = new int[array.length];
 		int max = 0;
-		for(int i = 0; i < array.length; i ++){
-			if(max < array[i]){
+		for (int i = 0; i < array.length; i++) {
+			if (max < array[i]) {
 				max = array[i];
 			}
 		}
 		int exp = String.valueOf(max).length();
-		for(int i = 0;  i < exp; i++){
-			for(int j = 0; j < array.length; j++){
-				
+		for (int i = 0; i < exp; i++) {
+			for (int j = 0; j < array.length; j++) {
+
 			}
 		}
 		return array;
